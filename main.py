0e242ba3b83ae7ca9089c66613e87c253e6a5933
shreedev96 <shreejithajith@gmail.com>
2026-05-30 22:03:01 +0530

    Initial commit: Completed Logging and ENV setup

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI,HTTPException,Depends
+import logging
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI,HTTPException,Depends,Header
 from fastapi.responses import StreamingResponse
 import schemas
 import models
@@ -7,8 +10,14 @@ from database import SessionLocal,engine
 import io
 import csv
 
-models.Base.metadata.create_all(bind=engine)
+load_dotenv()
+API_KEY=os.getenv("SECRET_API_KEY")
 
+logging.basicConfig(level=logging.INFO,format="%(asctime)s - %(levelname)s - %(message)s")
+logger=logging.getLogger(__name__)
+logger.info("Starting API.........")
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -23,7 +32,7 @@ def get_db():
 def read_root():
     return {"message": "Welcome to the Investment Portfolio API!"}
 
-@app.post("/investment")
+@app.post("/investment/add")
 def create_investment(new_investment:schemas.Investment,db:Session=Depends(get_db)):
     db_item=models.DBInvestment(name=new_investment.name,
                                 investment_type=new_investment.type,
@@ -31,10 +40,12 @@ def create_investment(new_investment:schemas.Investment,db:Session=Depends(get_d
     db.add(db_item)
     db.commit()
     db.refresh(db_item)  
+    logger.info(f"Successfully created new investment: {new_investment.name} for ${new_investment.amount}")
     return{
         "message":"Investment Received Successfully",
         "data":db_item
     }
+
 @app.get("/investment/exportToCsv")
 def get_investments_csv(db:Session=Depends(get_db)):
     db_items=db.query(models.DBInvestment).all()
@@ -54,6 +65,7 @@ def get_investments_csv(db:Session=Depends(get_db)):
     }    
 
     return StreamingResponse(output,media_type="text/csv",headers=headers)
+
 @app.get("/investment/getAll")
 def show_all(db:Session=Depends(get_db)):
     db_items=db.query(models.DBInvestment).all()
@@ -68,7 +80,12 @@ def get_an_investment(investment_id:int,db:Session=Depends(get_db)):
     return db_item
 
 @app.delete("/investment/delete/{investment_id}")
-def delete_investment(investment_id:int,db:Session=Depends(get_db)):
+def delete_investment(investment_id:int,db:Session=Depends(get_db),x_api_key: str=Header(default=None)):
+
+    if x_api_key != API_KEY:
+        logger.warning("The API KEY RECEIVED IS INVALID")
+        raise HTTPException(status_code=401,detail="couldn't authorise the API KEY as the Key is invalid")
+    
     db_item=db.query(models.DBInvestment).filter(models.DBInvestment.id==investment_id).first()
     if db_item is None:
         raise HTTPException(status_code=404,detail=f"Couldn't find an investment of the id {investment_id}")
